518c94f30ffa9ef802d1acaed82fd057077791d1
Kymani Stephens <kymanist00@gmail.com>
2023-04-14 13:10:40 -0700

    update Person constructor to include parameters

---
+++ src/js/person.js
@@ -0,0 +1,10 @@
+
+export default class Person {
+  constructor(age, lastBdayAge, futureBdayAge) {
+    this.age = age;
+    this.lastBdayAge = lastBdayAge;
+    this.futureBdayAge = futureBdayAge;
+  }
+
+  
+}
