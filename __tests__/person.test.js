cc081f62823d155d0677570b994916fab5f2ca3b
Kymani Stephens <kymanist00@gmail.com>
2023-04-14 12:43:30 -0700

    good fail for 1st test Person

---
+++ __tests__/person.test.js
@@ -0,0 +1,10 @@
+import Person from '../src/js/person.js';
+
+
+describe(Person, () => {
+
+  test('should return inputted age of new Person Object', () => {
+    const Kymani = new Person(22);
+    expect(Kymani.age).toEqual(23);
+  });
+});
